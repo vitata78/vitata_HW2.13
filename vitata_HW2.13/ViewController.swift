0afc2e70b0e5c8ca5d06b0f9662cbe0d18430266
Andrew Tolstov <andrew.tolstov@gmail.com>
2020-07-01 15:29:32 -0400

    Initial Commit

---
+++ vitata_HW2.13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vitata_HW2.13
+//
+//  Created by Andrew Tolstov on 7/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
